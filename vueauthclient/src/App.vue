3cbc92ce990c7657f4e5e2a3460794a543afa2f5
charless-dev <cagudelo20@gmail.com>
2019-01-23 21:16:39 -0500

    initial commit

---
+++ vueauthclient/src/App.vue
@@ -0,0 +1,27 @@
+<template>  
+    <div id="app">   
+        <div>    
+            <router-link :to="{ name: 'Dashboard'}">Dashboard</router-link>    
+            <router-link :to="{ name: 'Login'}">Login</router-link>    
+            <a href="#" v-on:click="logout">Logout</a>    
+        </div>    
+        <router-view/>    
+    </div>
+</template>  
+
+<script> 
+  import axios from "axios"  
+  import router from './router'  
+  export default {
+    name: "App",      
+    methods: {    
+      logout: function (e) {  
+        axios.get("/api/logout")
+        .then(() => {
+          console.log('bye');
+          router.push("/");
+        })
+      }
+    }
+  }
+</script>
---
+++ vueauthclient/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from "@/components/Login"
+import Dashboard from "@/components/Dashboard"
+import App from "@/App" 
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Login",
+      component: Login
+    },
+	{
+	    path: "/dashboard",
+	    name: "Dashboard",
+	    component: Dashboard
+	}        
+  ]
+})
